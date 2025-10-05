d8657ffc37d534b4a11f9aa838d619023b2678a2
zhangzhimingwork <56144141+zhangzhimingwork@users.noreply.github.com>
2025-10-05 10:27:10 +0800

    Update crypto utils for Cloudflare Workers

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -1,10 +1,9 @@
-const crypto = require('crypto');
-
 /**
- * 生成随机nonce
+ * 生成随机 nonce (Cloudflare Workers 版本)
+ * 使用 Web Crypto API
  */
-function generateNonce() {
-  return crypto.randomBytes(32).toString('hex');
+export function generateNonce() {
+  const array = new Uint8Array(32);
+  crypto.getRandomValues(array);
+  return Array.from(array, byte => byte.toString(16).padStart(2, '0')).join('');
 }
-
-module.exports = { generateNonce };
